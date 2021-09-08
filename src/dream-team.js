fb3a854cff95e1f805ae70abf07146558a9536d3
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-09-08 19:40:44 +0300

    fix: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let teamName = new Array();
+  members.forEach(item => {
+    if (typeof item === 'string') {
+      item = item.trim().toUpperCase();
+      teamName.push(item[0]);
+    }
+  });
+
+  return teamName.sort().join('');
 }
