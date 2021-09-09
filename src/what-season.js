19057025c55ef9bf6820b7a01b046ffac077d249
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-09-09 10:40:44 +0300

    fix: implement what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,7 @@
 import { NotImplementedError } from '../extensions/index.js';
 
+const INCORRECT_RESULT_MSG = 'Invalid date!';
+
 /**
  * Extract season from given date and expose the enemy scout!
  * 
@@ -11,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  let season = '';
+  let monthNumber = -1;
+  if (date == undefined) {
+    season = 'Unable to determine the time of year!';
+  }
+
+  try {
+    monthNumber = date.getMonth();
+  } catch (e) {
+    throw new Error(INCORRECT_RESULT_MSG);
+  }
+
+  if (monthNumber >= 2 && monthNumber <= 4) {
+    season = 'spring';
+  } else if (monthNumber >= 5 && monthNumber <= 7) {
+    season = 'summer';
+  } else if (monthNumber >= 8 && monthNumber <= 10) {
+    season = 'autumn';
+  } else {
+    season = 'winter';
+  }
+
+  return season;
 }
