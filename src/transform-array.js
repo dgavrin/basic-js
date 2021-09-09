9022bb4a022079246c980ea48e6d9ce06e1fa10f
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-09-09 18:43:01 +0300

    fix: comlete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,43 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    switch (item) {
+      case "--discard-next":
+        if (i != arr.length - 1) {
+          i++;
+        }
+        break;
+
+      case "--discard-prev":
+        if (i != 0 && arr[i - 2] != "--discard-next") {
+          newArr.pop();
+        }
+        break;
+
+      case "--double-next":
+        if (i != arr.length - 1) {
+          newArr.push(arr[i + 1]);
+        }
+        break;
+
+      case "--double-prev":
+        if (i != 0 && arr[i - 2] != "--discard-next") {
+          newArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        newArr.push(item);
+    }
+  }
+
+  return newArr;
 }
