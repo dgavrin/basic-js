44dcc36632bd92d48ca9a11b07cbccb073948c7c
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-09-10 22:22:45 +0300

    fix: complete vigenere-chiper task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,34 @@
 import { NotImplementedError } from '../extensions/index.js';
 
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,12 +49,66 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  direction = true;
+  constructor(value) {
+    if (value === false) {
+      this.direction = false;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encryptedMessage = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      let currentCharacter = message[i];
+      if (alphabet.indexOf(currentCharacter) == -1) {
+        encryptedMessage += currentCharacter;
+        continue;
+      }
+
+      let m = alphabet.indexOf(currentCharacter);
+      let k = alphabet.indexOf(key[j]);
+      encryptedMessage += String.fromCharCode(((m + k) % 26) + 65);
+      j === key.length - 1 ? j = 0 : j++;
+    }
+
+    if (!this.direction) {
+      return encryptedMessage.split("").reverse().join("");
+    }
+
+    return encryptedMessage;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encryptedMessage = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      let currentCharacter = message[i];
+      if (alphabet.indexOf(currentCharacter) == -1) {
+        encryptedMessage += currentCharacter;
+        continue;
+      }
+
+      let c = alphabet.indexOf(currentCharacter);
+      let k = alphabet.indexOf(key[j]);
+      encryptedMessage += String.fromCharCode(((c + 26 - k) % 26) + 65);
+      j === key.length - 1 ? j = 0 : j++;
+    }
+
+    if (!this.direction) {
+      return encryptedMessage.split("").reverse().join("");
+    }
+
+    return encryptedMessage;
   }
 }
